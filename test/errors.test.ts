c81d4cda41638fc1fd119c1513ad2b74ff4616bd
v1rtl <pilll.pl22@gmail.com>
2021-06-13 13:22:52 +0300

    bump deps

--- test/errors.test.ts
+++ test/errors.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, run, beforeEach, expect } from 'https://deno.land/x/tincan/mod.ts'
 import fetch from '../mod.ts'
-import { createServer, Server } from 'https://deno.land/x/node_http@0.0.7/mod.ts'
+import { createServer, Server } from 'https://deno.land/x/node_http@0.0.8/mod.ts'
 
 let server: Server,
   closed = 0
