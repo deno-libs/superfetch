15057f61b80c3f5a1bb80e24016210c73b9e4dec
v1rtl <pilll.pl22@gmail.com>
2023-02-14 22:09:49 +0200

    bump std

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as http from 'https://deno.land/std@0.151.0/node/http.ts'
-export * as assert from 'https://deno.land/std@0.151.0/node/assert.ts'
+export * as http from 'https://deno.land/std@0.177.0/node/http.ts'
+export * as assert from 'https://deno.land/std@0.177.0/node/assert.ts'
