bf5328034a767e8f12b767b721798622188ab145
v1rtl <yo@v1rtl.site>
2023-04-03 16:32:18 +0300

    tests for expectHeader

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,9 @@
-import { describe, it, run, expect } from 'https://deno.land/x/tincan@1.0.1/mod.ts'
+import {
+  describe,
+  expect,
+  it,
+  run,
+} from 'https://deno.land/x/tincan@1.0.1/mod.ts'
 import { makeFetch } from './mod.ts'
 import { Handler } from './types.ts'
 import { AssertionError } from 'https://deno.land/std@0.182.0/testing/asserts.ts'
@@ -36,7 +41,53 @@ describe('expectStatus', () => {
       res.expectStatus(404)
     } catch (e) {
       expect(e instanceof AssertionError).toBe(true)
-      expect((e as Error).message).toMatch('Values are not equal: expected to have status code 404 but was 200')
+      expect((e as Error).message).toMatch(
+        'Values are not equal: expected to have status code 404 but was 200',
+      )
+    }
+  })
+})
+
+describe('expectHeader', () => {
+  it('should pass with a correct header value', async () => {
+    const handler: Handler = () =>
+      new Response('Hello World', { headers: { 'Content-Type': 'text/plain' } })
+    const fetch = makeFetch(handler)
+    const res = await fetch('/')
+    res.expectHeader('Content-Type', 'text/plain')
+  })
+  it('supports regex', async () => {
+    const handler: Handler = () =>
+      new Response('Hello World', { headers: { 'Content-Type': 'text/plain' } })
+    const fetch = makeFetch(handler)
+    const res = await fetch('/')
+    res.expectHeader('Content-Type', /text/)
+  })
+  it('throws if value is incorrect', async () => {
+    const handler: Handler = () =>
+      new Response('Hello World', { headers: { 'Content-Type': 'text/html' } })
+    const fetch = makeFetch(handler)
+    const res = await fetch('/')
+    try {
+      res.expectHeader('Content-Type', 'text/plain')
+    } catch (e) {
+      expect(e instanceof AssertionError).toBe(true)
+      expect((e as Error).message).toMatch(
+        'Values are not equal: expected to have header Content-Type with value text/plain, got text/html',
+      )
+    }
+  })
+  it('throws if does not match regex', async () => {
+    const handler: Handler = () =>
+      new Response('Hello World', { headers: { 'Content-Type': 'text/html' } })
+    const fetch = makeFetch(handler)
+    const res = await fetch('/')
+    try {
+      res.expectHeader('Content-Type', /image/)
+    } catch (e) {
+      expect((e as Error).message).toMatch(
+        'Expected actual: "text/html" to match: "/image/": expected header Content-Type to match regexp /image/, got text/html',
+      )
     }
   })
 })
