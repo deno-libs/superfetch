26a342cfc483b6286c8f54090fcc37b2f130e4af
v1rtl <yo@v1rtl.site>
2023-08-03 21:48:19 +0300

    fix: types

--- types.ts
+++ types.ts
@@ -1,14 +1,6 @@
-/** Information about the connection a request arrived on. */
-export interface ConnInfo {
-  /** The local address of the connection. */
-  readonly localAddr: Deno.Addr
-  /** The remote address of the connection. */
-  readonly remoteAddr: Deno.Addr
-}
-
 export type Handler = (
   request: Request,
-  connInfo: ConnInfo,
+  connInfo: Deno.Conn
 ) => Response | Promise<Response>
 
 export type HandlerOrListener = Handler | Deno.Listener
